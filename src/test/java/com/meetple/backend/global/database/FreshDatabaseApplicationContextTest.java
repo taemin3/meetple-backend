3b12f8f819326afb2c8b3e585d0dcd04841a7b34
taemin Kim <ruhokim1@naver.com>
2026-09-22 21:19:23 +0900

    test: V21 마이그레이션 실행 수 반영

--- src/test/java/com/meetple/backend/global/database/FreshDatabaseApplicationContextTest.java
+++ src/test/java/com/meetple/backend/global/database/FreshDatabaseApplicationContextTest.java
@@ -109,7 +109,7 @@ class FreshDatabaseApplicationContextTest {
         assertThat(jdbcTemplate.queryForObject(
                 "SELECT COUNT(*) FROM flyway_schema_history WHERE success = TRUE",
                 Integer.class
-        )).isEqualTo(21);
+        )).isEqualTo(22);
         assertThat(jdbcTemplate.queryForObject(
                 """
                 SELECT version
--- src/test/java/com/meetple/backend/global/database/FreshDatabaseMigrationTest.java
+++ src/test/java/com/meetple/backend/global/database/FreshDatabaseMigrationTest.java
@@ -42,7 +42,7 @@ class FreshDatabaseMigrationTest {
 
         var firstMigration = flyway.migrate();
 
-        assertThat(firstMigration.migrationsExecuted).isEqualTo(21);
+        assertThat(firstMigration.migrationsExecuted).isEqualTo(22);
         assertThat(flyway.validateWithResult().validationSuccessful).isTrue();
 
         try (var connection = openConnection()) {
